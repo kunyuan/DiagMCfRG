d39bf55ed4e077c9db2990b6309e0ed164c56485
Kun Chen <chenkun@ustc.edu.cn>
2019-02-01 18:47:58 -0500

    dev

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,7 @@ void InitPara() {
   Para.GroupName = {"1", "2", "3", "3_1_0"};
   Para.ReWeight = {1.0, 1.0, 5.0, 1.0, 0.1};
   Para.SelfEnergyType = FOCK;
+  // Para.SelfEnergyType = BARE;
 
   //// initialize the global parameter //////////////////////
   double Kf;
