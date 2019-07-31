2c6a93b0d4bece3c75c2dc573103921acfe80d2f
Kun Chen <chenkun@ustc.edu.cn>
2019-07-31 13:40:35 -0400

    seems working now

--- src/global.h
+++ src/global.h
@@ -17,12 +17,12 @@ const bool DEBUGMODE = true;
 // D=2 or D=3
 const int D = 2;
 // number of q bins of the external momentum
-const int ExtMomBinSize = 32;
+const int ExtMomBinSize = 64;
 // number of bins for the angle between InL and InR legs
 const int AngBinSize = 16;
 // number of energy scales, only useful in RG approach
 const int ScaleBinSize = 64;
-const int TauBinSize = 128;
+const int TauBinSize = 256;
 
 enum selfenergy { BARE, FOCK, DRESSED }; // self energy type
 enum type { RG, POLAR };
